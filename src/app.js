f14804077244155e5ec28a520f5088b5b17f4fbd
ALCIDESCONTRERAS <alcidescon@gmail.com>
2024-11-28 08:16:49 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -7,5 +7,31 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+  let who = ["The dog", "My grandma", "The mailman", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "my phone", "the car"];
+  let when = [
+    "before the class",
+    "when I was sleeping",
+    "while I was exercising",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  function palabrasRamdom(lista) {
+    let palabras = "";
+    palabras = lista[Math.floor(Math.random() * lista.length)];
+    return palabras;
+  }
+
+  let generator = document.getElementById("excuses");
+  generator.innerHTML =
+    palabrasRamdom(who) +
+    " " +
+    palabrasRamdom(action) +
+    " " +
+    palabrasRamdom(what) +
+    " " +
+    palabrasRamdom(when);
 };
